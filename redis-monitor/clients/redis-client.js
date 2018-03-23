bbcbe8d066a2a7630066210ed23e246136ab539f
Alexandre Yembo <alexandre.leite@grupoltm.com.br>
2018-03-23 07:45:50 -0300

    create project structure

---
+++ redis-monitor/clients/redis-client.js
@@ -0,0 +1,21 @@
+const redis = require('redis'),
+bluebird = require('bluebird')
+
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+const createClient = (host, password) => redis.createClient({host, password})
+
+const subscribe = (client, channel, callback) => {
+  client.on('pmessage', (pattern, ch, expiredKey) => {
+    console.log(`key ${expiredKey} has expired: ${pattern}`)
+    callback(expiredKey)
+  })
+  console.log(`assigning ${channel} `)
+  client.psubscribe(channel)
+  console.log('assigned!')
+}
+
+const stopSubscribe = (client) => client.quit()
+
+module.exports = {createClient, subscribe, stopSubscribe}
---
+++ redis-monitor/source/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  REDIS_SERVER: process.env.REDIS_SERVER || '',
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD || '',
+  REDIS_DATA_BASE : process.env.REDIS_DATA_BASE || ''
+}
